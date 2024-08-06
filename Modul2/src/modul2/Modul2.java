4e953e34f2e99dc9f1226e20b3eb1039195c98ee
Febirahmas <136708341+Febirahmas@users.noreply.github.com>
2024-08-06 15:32:48 +0700

    Modul 2

---
+++ Modul2/src/modul2/Modul2.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package modul2;
+
+/**
+ *
+ * @author PC RPL - R1
+ */
+public class Modul2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        /*Tugas modul=new Tugas();
+        modul.setVisible(true);*/
+        
+        /*Tugas2 modul=new Tugas2();
+        modul.setVisible(true);*/
+    }
+    
+}
